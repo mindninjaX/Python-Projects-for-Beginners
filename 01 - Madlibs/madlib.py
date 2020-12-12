d76ac7e72185e5436cad6b737638391a6781757c
Rishabh Singh <rishabhsinghs90s@gmail.com>
2020-12-12 14:10:29 +0530

    Added 01 project

---
+++ 01 - Madlibs/madlib.py
@@ -0,0 +1,10 @@
+# Variables to store user prompts
+noun = input("Noun: ")
+adj = input("Adjective: ")
+noun2 = input("Noun: ")
+
+# Creating a string var to store madlib sentence
+madlib = f"After hiding the painting in his {noun} for two years, he grew {adj} and tried to sell it to a {noun2} in Mumbai, but was caught"
+
+# Printing the madlib
+print(madlib)
