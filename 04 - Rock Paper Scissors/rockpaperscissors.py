1f65f5a7d193ec0a958341c31435e53316e01abb
Rishabh Singh <rishabhsinghs90s@gmail.com>
2020-12-13 22:01:30 +0530

    Added project 04

---
+++ 04 - Rock Paper Scissors/rockpaperscissors.py
@@ -0,0 +1,21 @@
+import random
+
+def rock_paper_scissors():
+    player = input("What is your choice - 'r' for rock, 's' for scissor, 'p' for paper: ")
+    opponent = random.choice(['r','s','p'])
+
+    if player == opponent:
+        return print(f"Its a Tie! Choice is {opponent}")
+
+    if check_win(player, opponent):
+        return print(f"Yay! you won! Choice is {opponent}")
+    
+    if check_win(player, opponent) != True:
+        return print(f"You lost! Choice is {opponent}")
+
+    
+def check_win(user, computer):
+    if (user == 'r' and computer == 's') or (user == 's' and computer == 'p') or (user == 'p' and computer == 'r'):
+        return True
+
+rock_paper_scissors()
