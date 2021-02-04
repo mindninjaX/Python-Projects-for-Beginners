c5f3a36c3f518e334115cf5a192a05165f4231f0
Rishabh Singh <rishabhsinghs90s@gmail.com>
2021-02-04 06:20:23 +0530

    minor changes in acronym creator

--- Acronyms Creator/Acronyms Creator.py
+++ Acronyms Creator/Acronyms Creator.py
@@ -1,8 +1,8 @@
 # Taking user input 
 user_input = input("Enter a phrase: ")
 
-# Spiliting the user input into individual words using split() method
-phrase = user_input.split()
+# Getting rid of 'of' word using replace() method & Spiliting the user input into individual words using split() method
+phrase = (user_input.replace('of', '')).split()
 
 # Initializing an empty string to append the acronym
 a = ""
@@ -11,4 +11,4 @@ a = ""
 for word in phrase:
     a = a + word[0].upper()
 
-print(a)
+print(f'Acronym of {user_input} is {a}')
