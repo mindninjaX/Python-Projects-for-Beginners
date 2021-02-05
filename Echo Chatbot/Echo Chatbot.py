acbe720b2a48fff63e236238070aae5f39a0935c
Rishabh Singh <rishabhsinghs90s@gmail.com>
2021-02-05 20:44:36 +0530

    add 3 new projects

---
+++ Echo Chatbot/Echo Chatbot.py
@@ -0,0 +1,8 @@
+print("Let's talk! Enter 'quit' to exit...")
+
+while True:
+    user = input("You: ")
+    print(f"Bot: {user}")
+
+    if user == 'quit':
+        break
---
+++ Fahrenheit to Celsius Converter/Fahrenheit to Celsius Converter.py
@@ -0,0 +1,5 @@
+temp = float(input("Enter temperature in Fahrenheit: "))
+
+celsius = (temp - 32) * 5/9
+
+print(celsius)
