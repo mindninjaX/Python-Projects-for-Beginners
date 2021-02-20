153830f52ab5afedd42f8dfdc798ca2fe480d944
Rishabh Singh <rishabhsinghs90s@gmail.com>
2021-02-20 23:26:27 +0530

    a small fix

--- Guess the number (computer)/guessthenumber_computer.py
+++ Guess the number (computer)/guessthenumber_computer.py
@@ -1,14 +1,13 @@
 import random
 
-def guess(num):
-    random_number = random.randint(1, num)
-    guess = 0
-    while guess != random_number:
-        guess = int(input(f"Guess the number between 1 & {num}: "))
-        if guess < random_number:
-            print("Wrong! Too low...")
-        elif guess > random_number:
-            print("Wrong! Too high...")
-    print(f"Thats Right! Random number is {random_number}")
+max_num = 30
 
-guess(10)
+random_number = random.randint(1, max_num)
+guess = 0
+while guess != random_number:
+    guess = int(input(f"Guess the number between 1 & {max_num}: "))
+    if guess < random_number:
+        print("Wrong! Too low...")
+    elif guess > random_number:
+        print("Wrong! Too high...")
+print(f"Thats Right! Random number is {random_number}")
